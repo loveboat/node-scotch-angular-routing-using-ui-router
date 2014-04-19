92847fc799cac5379ea30916a0b11fec7d3217de
David Lusby <lusbyd@gmail.com>
2014-04-19 14:41:21 +0100

    Implemented a couple of nested views using ui-route and partials

--- public/app.js
+++ public/app.js
@@ -12,6 +12,22 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
             url: '/home',
             templateUrl: 'partial-home.html'
         })
+
+        // nested list with custom controller
+        .state('home.list', {
+            url: '/list',
+            templateUrl: 'partial-home-list.html',
+            controller: function($scope) {
+                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+            }
+        })
+
+        // nested list with just from random string data
+        .state('home.paragraph', {
+            url: '/paragraph',
+            template: 'I could sure use a drink right now.'
+        })
+
         
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
